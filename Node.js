d11348fb7fc29816e86d33bcabc979f35be0b8d5
Hex-Umbra <g.p.dacosta@hotmail.com>
2025-09-25 15:34:02 +0200

    A* Algorithme avec une gridMap comme test. -Fonctionnalités  - Recherche de Chemin -Surlignage du processus de check de chaque Noeuds jusqu'à Arrivé - Pause et Start pendant le chemin - Délai de recherche entre chaque nouveau noeud au choix

--- Node.js
+++ Node.js
@@ -4,14 +4,18 @@ class Node {
     this.x = x;
     this.y = y;
 
-    // Cost from starting point to this node
+    // Cost from starting point to current node
     this.g = 0;
     // Heuristic cost from current node to end
     this.h = 0;
-    // Total cost
+    // Total cost, Sum of g and h
     this.f = 0;
 
     // Parent node in the path
     this.parent = null;
+
+    // Whether the node has been visited
+    this.visited = false;
   }
+
 }
